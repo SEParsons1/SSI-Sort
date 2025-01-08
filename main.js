165719d3626395e54c836a6f18813a4e2ef2025b
SEParsons1 <sparsonsbc@outlook.com>
2025-01-07 20:28:30 -0800

    main.js

--- main.js
+++ main.js
@@ -39,7 +39,7 @@ function fitAreaDisplayText() {
 
   // Make it just a tad smaller after it fits
   if (fontSize > 0) {
-    fontSize -= 2; // Adjust this value if you want more or less reduction
+    fontSize -= 4; // Adjust this value if you want more or less reduction
     areaDisplay.style.fontSize = fontSize + 'px';
   }
 }
